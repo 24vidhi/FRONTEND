ae5a0efa21a97c46219ae1937130708186ac60fa
Vidhi Kumari <vidhisingh14kta@gmail.com>
2024-03-31 17:27:42 +0530

    Nav bar is added to every page

--- src/App.vue
+++ src/App.vue
@@ -4,5 +4,19 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <!-- NAV BAR == present in all the pages -->
+  <v-card
+    color="grey-lighten-6"
+    rounded="0"
+    flat
+  >
+    <v-toolbar color="blue-lighten-4" density="compact">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Chat App</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+    </v-toolbar>
+  </v-card>
   <RouterView />
 </template>
